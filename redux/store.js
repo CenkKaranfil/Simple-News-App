6d3a54f02e3b6fd31fc230b15cd0389aec4bc387
CenkKaranfil <cenkkaranfil@hotmail.com>
2024-01-14 17:07:06 +0300

    first update

--- redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartReducer from "./CartReducer";
-
-export const store = configureStore({
-    reducer:{
-        cart: CartReducer,
-    },
-});
