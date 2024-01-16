78810b14dcc99867c707a673c28f18a0bb3fca77
CenkKaranfil <cenkkaranfil@hotmail.com>
2024-01-16 16:40:28 +0300

    improved overall quality of the app

--- Components/DetailScreen.js
+++ Components/DetailScreen.js
@@ -1,35 +1,62 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { Alert, View, Text, Image, StyleSheet, Linking, TouchableOpacity, ScrollView } from 'react-native';
 import * as React from 'react';
+import { useCallback } from 'react';
 
 const DetailScreen = ({ route }) => {
 
   const { article } = route.params;
 
+  const OpenURLButton = ({ url }) => {
+
+    const handlePress = useCallback(async () => {
+
+      const supported = await Linking.canOpenURL(url);
+
+      if (supported) {
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(`Cannot open: ${url}`);
+      }
+
+    }, [url]);
+
+    return <TouchableOpacity
+      style={styles.button}
+      onPress={handlePress}
+    >
+      <Text style={styles.link}>
+        Haberin Devamı
+      </Text>
+    </TouchableOpacity>
+
+  };
 
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: article.image_url }} ></Image>
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>
-          {article.title}
-        </Text>
-        <Text style={styles.text}>
-          {article.description}
-        </Text>
-        <Text style={styles.text}>
-          {article.snippet}
-        </Text>
-        <Text style={styles.text}>
-          {article.source}
-        </Text>
-        <Text style={styles.text}>
-          {article.published_at}
-        </Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: article.image }} ></Image>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>
+            {article.title}
+          </Text>
+          <Text style={styles.desc}>
+            {article.description}
+          </Text>
+          <View style={styles.innerBox}>
+            <Text style={styles.author}>
+              {article.author}
+            </Text>
+            <Text style={styles.date}>
+              {article.published}
+            </Text>
+            <OpenURLButton url={article.url}>Haberin Devamı</OpenURLButton>
+
+          </View>
+        </View>
+
       </View>
 
-    </View>
-
-
+    </ScrollView>
 
 
   )
@@ -43,9 +70,74 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
 
-  }
+  },
+  textContainer: {
+    flex: 1,
+    marginTop: 40,
+    marginStart: 10,
+    marginEnd: 10,
+    marginBottom:20,
+
+  },
+
+  image: {
+
+    width: 400,
+    height: 400,
+  },
+
+  title: {
+    fontSize: 20,
+
+
+  },
+  desc: {
+    marginTop: 20,
+    fontSize: 18,
+
+
+  },
+
+  button: {
+    borderColor: "black",
+    borderWidth: 1,
+    backgroundColor: "white",
+    borderRadius: 6,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    textAlign: 'center',
+    marginTop: 20,
+    marginRight: 6,
+    paddingVertical:6,
+    paddingHorizontal:2,
+    justifyContent: 'center',
+    maxWidth: "50%",
+    alignItems: 'center',
+  },
+
+  link: {
+    fontSize: 20,
+    color: "black"
+  },
+
+  author: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "gray",
+  },
+
+  date: {
+    marginTop: 10,
+    color: "gray",
+  },
 
 
 
